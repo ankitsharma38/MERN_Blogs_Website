5fa6b8330491a4e1f01e2be4dbd65b8fec18c539
ankitsharma38 <ankitsharma7805@gmail.com>
2025-03-07 17:58:35 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='text-sm text-center bg-gray-50 py-4'>
-            © Copyright 2024 | Designed & Developed By: <a href='https://www.linkedin.com/in/ankit-sharma-08b37a26a/' className='font-bold'>Ankit Sharma</a>
+            © Copyright 2024 | Designed & Developed By: <a href='https://www.linkedin.com/in/krokxx03/' className='font-bold'>Abhishek Kumar & Ankit Sharma</a>
         </div>
     )
 }
